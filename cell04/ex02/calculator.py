65c1528a4aa723b448a1edbdd6a0bb17f43e8d1e
Newsosad <nmaskarnitya@gmail.com>
2024-10-11 10:20:35 +0700

    Create calculator.py

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+n = input("Give me the first number: ")
+n1 = int(n)
+n2 = input("Give me the first number: ")
+n3 = int(n2)
+print("Thank you! ")
+num1= n1 + n3
+num2= n1 - n3
+num3= n1 / n3
+num4= n1 * n3
+print(n1 , "+" , n3 , "=" , num1)
+print(n1 , "-" , n3 , "=" , num2)
+print(n1 , "/" , n3 , "=" , num3)
+print(n1 , "*" , n3 , "=" , num4)
